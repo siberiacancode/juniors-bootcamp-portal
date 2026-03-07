eb848e68344c21592a94955f59d1a8c0ca444f61
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2026-03-07 14:45:54 +0500

    layout 🌠 fix footer

--- app/(components)/layout/Footer/Footer.tsx
+++ app/(components)/layout/Footer/Footer.tsx
@@ -75,115 +75,104 @@ const OPENSOURCE = [
   }
 ] as const;
 
-const TopPart = () => {
-  return (
-    <div className='flex flex-col gap-10 rounded-2xl bg-muted px-4 py-6 sm:flex-row sm:flex-wrap sm:items-center sm:px-6 sm:py-10'>
-      <span className='font-pixelify-sans text-3xl font-bold'>juniorsbootcamp</span>
-
-      {PRODUCTS.map((product) => (
-        <span key={product.href} className='text-sm font-semibold'>
-          Ссылка на продукт
-        </span>
-      ))}
-    </div>
-  );
-};
-
-const BottomPart = () => {
+export const Footer = () => {
   const intl = useIntl();
 
   return (
-    <div className='space-y-6 rounded-2xl bg-muted px-4 py-6 sm:space-y-10 sm:px-6 sm:py-10'>
-      <div className='flex flex-col gap-6 sm:flex-row sm:gap-10'>
-        <div className='flex flex-col gap-4'>
-          <p className='font-normal'>
-            <IntlText path='footer.description' />
-          </p>
+    <footer className='mx-auto mb-12 w-full max-w-(--max-width) space-y-6 px-6 sm:mb-16'>
+      <div className='flex flex-col gap-10 rounded-2xl bg-muted px-4 py-6 sm:flex-row sm:flex-wrap sm:items-center sm:px-6 sm:py-10'>
+        <span className='font-pixelify-sans text-3xl font-bold'>juniorsbootcamp</span>
 
-          <Button asChild className='sm:w-fit'>
-            <Link href='https://t.me/siberiacancode' rel='noopener noreferrer' target='_blank'>
-              <IntlText path='link.contact' />
-            </Link>
-          </Button>
+        {PRODUCTS.map((product) => (
+          <span key={product.href} className='text-sm font-semibold'>
+            Ссылка на продукт
+          </span>
+        ))}
+      </div>
 
-          <div className='flex gap-6'>
-            {SOCIALS.map((social) => (
-              <Button asChild iconOnly key={social.href} variant='ghost'>
-                <Link href={social.href} rel='noopener noreferrer' target='_blank'>
-                  <social.Icon aria-label={intl.formatMessage({ id: social.alt })} />
-                </Link>
-              </Button>
-            ))}
+      <div className='space-y-6 rounded-2xl bg-muted px-4 py-6 sm:space-y-10 sm:px-6 sm:py-10'>
+        <div className='flex flex-col gap-6 sm:flex-row sm:gap-10'>
+          <div className='flex flex-col gap-4'>
+            <p className='font-normal'>
+              <IntlText path='footer.description' />
+            </p>
+
+            <Button asChild className='sm:w-fit'>
+              <Link href='https://t.me/siberiacancode' rel='noopener noreferrer' target='_blank'>
+                <IntlText path='link.contact' />
+              </Link>
+            </Button>
+
+            <div className='flex gap-6'>
+              {SOCIALS.map((social) => (
+                <Button asChild iconOnly key={social.href} variant='ghost'>
+                  <Link href={social.href} rel='noopener noreferrer' target='_blank'>
+                    <social.Icon aria-label={intl.formatMessage({ id: social.alt })} />
+                  </Link>
+                </Button>
+              ))}
+            </div>
+          </div>
+
+          <div className='space-y-4'>
+            <h3 className='font-bold'>
+              <IntlText path='footer.directions.title' />
+            </h3>
+            <ul className='space-y-3 text-sm font-medium'>
+              {NAVIGATION.map((navigation) => (
+                <li key={navigation.href}>
+                  <Link href={navigation.href}>
+                    <IntlText path={navigation.label} />
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className='space-y-4'>
+            <h3 className='font-bold'>
+              <IntlText path='footer.opensource.title' />
+            </h3>
+            <ul className='space-y-3 text-sm font-medium'>
+              {OPENSOURCE.map((opensource) => (
+                <li key={opensource.href}>
+                  <Link href={opensource.href} rel='noopener noreferrer' target='_blank'>
+                    {opensource.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
 
-        <div className='space-y-4'>
-          <h3 className='font-bold'>
-            <IntlText path='footer.directions.title' />
-          </h3>
-          <ul className='space-y-3 text-sm font-medium'>
-            {NAVIGATION.map((navigation) => (
-              <li key={navigation.href}>
-                <Link href={navigation.href}>
-                  <IntlText path={navigation.label} />
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <div className='flex flex-col gap-4 sm:flex-row sm:justify-between'>
+          <p className='text-sm font-normal text-muted-foreground'>
+            Built by{' '}
+            <Link
+              className='underline'
+              href='https://github.com/siberiacancode'
+              rel='noopener noreferrer'
+              target='_blank'
+            >
+              siberiacancode
+            </Link>
+            . The source code is available on{' '}
+            <Link
+              className='underline'
+              href='https://github.com/siberiacancode/juniors-bootcamp-portal'
+              rel='noopener noreferrer'
+              target='_blank'
+            >
+              GitHub
+            </Link>
+            .
+          </p>
 
-        <div className='space-y-4'>
-          <h3 className='font-bold'>
-            <IntlText path='footer.opensource.title' />
-          </h3>
-          <ul className='space-y-3 text-sm font-medium'>
-            {OPENSOURCE.map((opensource) => (
-              <li key={opensource.href}>
-                <Link href={opensource.href} rel='noopener noreferrer' target='_blank'>
-                  {opensource.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <p className='text-xs font-normal text-muted-foreground'>
+            Avatars by Alohe via Figma Community
+          </p>
         </div>
       </div>
-
-      <div className='flex flex-col gap-4 sm:flex-row sm:justify-between'>
-        <p className='text-sm font-normal text-muted-foreground'>
-          Built by{' '}
-          <Link
-            className='underline'
-            href='https://github.com/siberiacancode'
-            rel='noopener noreferrer'
-            target='_blank'
-          >
-            siberiacancode
-          </Link>
-          . The source code is available on{' '}
-          <Link
-            className='underline'
-            href='https://github.com/siberiacancode/juniors-bootcamp-portal'
-            rel='noopener noreferrer'
-            target='_blank'
-          >
-            GitHub
-          </Link>
-          .
-        </p>
-
-        <p className='text-xs font-normal text-muted-foreground'>
-          Avatars by Alohe via Figma Community
-        </p>
-      </div>
-    </div>
-  );
-};
-
-export const Footer = () => {
-  return (
-    <footer className='mx-auto mb-12 w-full max-w-(--max-width) space-y-6 px-6 sm:mb-16'>
-      <TopPart />
-      <BottomPart />
     </footer>
   );
 };
