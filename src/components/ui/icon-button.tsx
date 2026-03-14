9664b89dc453a025e6e2e98cb658b002801e5128
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2026-03-14 08:50:10 +0500

    guide 🌠 fix sizes

--- src/components/ui/icon-button.tsx
+++ src/components/ui/icon-button.tsx
@@ -7,7 +7,7 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const iconButtonVariants = cva(
-  `inline-flex shrink-0 items-center justify-center rounded-sm transition-all outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-danger aria-invalid:ring-danger/20 dark:aria-invalid:ring-danger/40 [&_svg]:pointer-events-none [&_svg]:shrink-0`,
+  `inline-flex shrink-0 items-center justify-center gap-2 rounded-sm transition-all outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-danger aria-invalid:ring-danger/20 dark:aria-invalid:ring-danger/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-5`,
   {
     variants: {
       variant: {
@@ -22,9 +22,9 @@ const iconButtonVariants = cva(
           'bg-danger text-white hover:bg-danger/90 focus-visible:ring-danger/20 dark:bg-danger/60 dark:focus-visible:ring-danger/40'
       },
       size: {
-        sm: `size-8 gap-1.5 [&_svg:not([class*='size-'])]:size-6`,
-        md: `size-10 gap-2 [&_svg:not([class*='size-'])]:size-6`,
-        lg: `size-13 gap-2 [&_svg:not([class*='size-'])]:size-9`
+        sm: `size-8`,
+        md: `size-10`,
+        lg: `size-13`
       }
     },
     defaultVariants: {
