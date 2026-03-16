257e2cc9cf6908c55ad21d4cffdee9743fdc9d4b
debabin <dmitriybabinup@gmail.com>
2026-03-16 13:39:00 +0700

    main 🌀 fix build

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ import remarkDirective from 'remark-directive';
 import remarkGfm from 'remark-gfm';
 import { visit } from 'unist-util-visit';
 
-import { OPTIONS_MULTIPLE_THEMES, SUPPORTED_LANGUAGES } from '@/markdown/shiki';
+import { OPTIONS_MULTIPLE_THEMES, SUPPORTED_LANGUAGES } from './src/markdown/shiki';
 
 function remarkCodeGroup() {
   return (tree: any) => {
