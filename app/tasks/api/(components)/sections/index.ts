e62f3a48123dd7bf0d869666b9efe6437ab23055
debabin <dmitriybabinup@gmail.com>
2026-03-16 22:42:41 +0700

    main 🌀 hide pages

--- app/tasks/api/(components)/sections/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ApiTypesSection/ApiTypesSection';
-export * from './AuthenticationSection/AuthenticationSection';
-export * from './FAQSection/FAQSection';
-export * from './GoalsSection/GoalsSection';
