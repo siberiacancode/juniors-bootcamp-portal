996a154446817a4b57879b909eb1db3e2e0550a3
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2026-03-31 18:25:52 +0500

    task-page 🌠 removed unused components

--- app/provider.tsx
+++ app/provider.tsx
@@ -3,8 +3,6 @@
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import * as React from 'react';
 
-import { TooltipProvider } from '@/components/ui';
-
 import type { IntlProviderProps } from './(contexts)/intl';
 
 import { IntlProvider } from './(contexts)/intl';
@@ -18,9 +16,7 @@ interface ProviderProps {
 export const Provider = ({ children, intl }: ProviderProps) => (
   <IntlProvider {...intl}>
     <ThemeProvider>
-      <NuqsAdapter>
-        <TooltipProvider>{children}</TooltipProvider>
-      </NuqsAdapter>
+      <NuqsAdapter>{children}</NuqsAdapter>
     </ThemeProvider>
   </IntlProvider>
 );
