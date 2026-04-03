4cba86a6aee3d3c1990d5b1969a52e65109bcb0b
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2026-04-03 22:18:06 +0500

    ssr-task 🌠 cookie consent: fix metric condition & off

--- app/_components/CookieConsent/hooks/useCookieConsent.ts
+++ app/_components/CookieConsent/hooks/useCookieConsent.ts
@@ -3,15 +3,17 @@ import { useCookie } from '@siberiacancode/reactuse';
 import { COOKIES } from '@/constants';
 
 const PATH = '/';
+const MAX_AGE = 365 * 24 * 60 * 60;
 
 export const useCookieConsent = () => {
   const cookieConsent = useCookie(COOKIES.COOKIE_CONSENT, {
     path: PATH,
-    initialValue: false
+    initialValue: false,
+    maxAge: MAX_AGE
   });
 
   return {
     hasConsent: cookieConsent.value,
-    accept: () => cookieConsent.set(true, { path: PATH })
+    accept: () => cookieConsent.set(true, { path: PATH, maxAge: MAX_AGE })
   };
 };
