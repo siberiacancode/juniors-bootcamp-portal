a40ef8c86656ea1892946137c40162b1ab576939
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2026-04-03 22:18:06 +0500

    ssr-task 🌠 cookie consent: fix metric condition & off

--- app/_components/CookieConsent/CookieConsent.tsx
+++ app/_components/CookieConsent/CookieConsent.tsx
@@ -11,7 +11,7 @@ export const CookieConsent = () => {
   return (
     <>
       {!hasConsent && <CookieConsentPopover />}
-      {!hasConsent && isProduction && <YandexMetrikaScript />}
+      {hasConsent && isProduction && <YandexMetrikaScript />}
     </>
   );
 };
