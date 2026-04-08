5491c58c6d33038970d5fd8940c2f486a11e2239
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2026-04-08 19:05:00 +0500

    landing 🌠 add marquee

---
+++ src/components/ui/marquee.tsx
@@ -0,0 +1,52 @@
+import type { ComponentProps } from 'react';
+
+import { cn } from '@/lib/utils';
+
+interface MarqueeProps extends ComponentProps<'div'> {
+  pauseOnHover?: boolean;
+  /**
+   * Number of times the children should be repeated to fill the marquee.
+   * @default 4
+   */
+  repeat?: number;
+  reverse?: boolean;
+  /**
+   * Animation duration in seconds.
+   * @default 30
+   */
+  speed?: number;
+}
+
+export const Marquee = ({
+  className,
+  reverse = false,
+  pauseOnHover = false,
+  children,
+  repeat = 4,
+  speed = 30,
+  ...props
+}: MarqueeProps) => (
+  <div className={cn('group overflow-hidden', className)} {...props}>
+    <div
+      className={cn(
+        'inline-flex animate-[marquee_linear_infinite] will-change-transform',
+        pauseOnHover && 'group-hover:paused',
+        reverse && 'direction-[reverse]'
+      )}
+      style={{
+        animationDuration: `${speed}s`
+      }}
+    >
+      {Array.from({ length: repeat }).map((_, index) => (
+        <div
+          // eslint-disable-next-line react/no-array-index-key
+          key={index}
+          {...(index !== 0 && { 'aria-hidden': true, role: 'presentation' })}
+          className={cn('flex shrink-0 gap-6 pr-6')}
+        >
+          {children}
+        </div>
+      ))}
+    </div>
+  </div>
+);
