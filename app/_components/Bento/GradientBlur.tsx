a5fda2a63dd1666d29cc71824ef8e5a6a6332f4b
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2026-04-11 22:19:54 +0500

    landing 🌠 work in progress

---
+++ app/_components/Bento/GradientBlur.tsx
@@ -0,0 +1,243 @@
+import type { SVGProps } from 'react';
+
+export const GradientBlur = (props: SVGProps<SVGSVGElement>) => (
+  <svg viewBox='0 0 328 220' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <g clipPath='url(#a)'>
+      <g filter='url(#b)'>
+        <ellipse
+          className='[--b:#c7c5ff] dark:[--b:#8a88cc]'
+          cx='25'
+          cy='50'
+          fill='var(--b)'
+          rx='78'
+          ry='97'
+        />
+      </g>
+      <g filter='url(#c)'>
+        <ellipse
+          className='[--c:#d8adff] dark:[--c:#9d66d6]'
+          cx='141'
+          cy='50'
+          fill='var(--c)'
+          rx='86'
+          ry='41'
+        />
+      </g>
+      <g filter='url(#d)'>
+        <ellipse
+          className='[--d:#f7a5fd] dark:[--d:#b860bd]'
+          cx='233'
+          cy='45'
+          fill='var(--d)'
+          rx='92'
+          ry='74'
+        />
+      </g>
+      <g filter='url(#e)'>
+        <ellipse
+          className='[--e:#cec7ff] dark:[--e:#8a82cc]'
+          cx='120'
+          cy='85'
+          fill='var(--e)'
+          rx='69'
+          ry='83'
+        />
+      </g>
+      <g filter='url(#f)'>
+        <ellipse
+          className='[--f:#d3cfff] dark:[--f:#8d87cc]'
+          cx='247'
+          cy='83'
+          fill='var(--f)'
+          rx='69'
+          ry='94'
+        />
+      </g>
+      <g filter='url(#g)'>
+        <ellipse
+          className='[--g:#dbb4ff] dark:[--g:#9668cc]'
+          cx='209'
+          cy='77'
+          fill='var(--g)'
+          rx='106'
+          ry='68'
+        />
+      </g>
+      <g filter='url(#h)'>
+        <ellipse
+          className='[--h:#ffa8ff] dark:[--h:#cc6bcc]'
+          cx='101'
+          cy='9'
+          fill='var(--h)'
+          rx='88'
+          ry='56'
+        />
+      </g>
+      <g filter='url(#i)'>
+        <ellipse
+          className='[--i:#fda1ff] dark:[--i:#c45dcc]'
+          cx='234'
+          cy='22'
+          fill='var(--i)'
+          rx='93'
+          ry='69'
+        />
+      </g>
+      <g filter='url(#j)'>
+        <ellipse
+          className='[--j:#fed7ff] dark:[--j:#c088c0]'
+          cx='234'
+          cy='4'
+          fill='var(--j)'
+          rx='93'
+          ry='51'
+        />
+      </g>
+      <g filter='url(#k)'>
+        <ellipse
+          className='[--k:#ffdbff] dark:[--k:#c48fc4]'
+          cx='111'
+          cy='-8'
+          fill='var(--k)'
+          rx='80'
+          ry='57'
+        />
+      </g>
+    </g>
+    <defs>
+      <filter
+        colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
+        height='258'
+        id='b'
+        width='220'
+        x='-85'
+        y='-79'
+      >
+        <feFlood floodOpacity='0' result='BackgroundImageFix' />
+        <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape' />
+        <feGaussianBlur result='effect1_foregroundBlur_40004022_5050' stdDeviation='16' />
+      </filter>
+      <filter
+        colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
+        height='188'
+        id='c'
+        width='278'
+        x='2'
+        y='-44'
+      >
+        <feFlood floodOpacity='0' result='BackgroundImageFix' />
+        <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape' />
+        <feGaussianBlur result='effect1_foregroundBlur_40004022_5050' stdDeviation='26.5' />
+      </filter>
+      <filter
+        colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
+        height='254'
+        id='d'
+        width='290'
+        x='88'
+        y='-82'
+      >
+        <feFlood floodOpacity='0' result='BackgroundImageFix' />
+        <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape' />
+        <feGaussianBlur result='effect1_foregroundBlur_40004022_5050' stdDeviation='26.5' />
+      </filter>
+      <filter
+        colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
+        height='230'
+        id='e'
+        width='202'
+        x='19'
+        y='-30'
+      >
+        <feFlood floodOpacity='0' result='BackgroundImageFix' />
+        <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape' />
+        <feGaussianBlur result='effect1_foregroundBlur_40004022_5050' stdDeviation='16' />
+      </filter>
+      <filter
+        colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
+        height='252'
+        id='f'
+        width='202'
+        x='146'
+        y='-43'
+      >
+        <feFlood floodOpacity='0' result='BackgroundImageFix' />
+        <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape' />
+        <feGaussianBlur result='effect1_foregroundBlur_40004022_5050' stdDeviation='16' />
+      </filter>
+      <filter
+        colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
+        height='242'
+        id='g'
+        width='318'
+        x='50'
+        y='-44'
+      >
+        <feFlood floodOpacity='0' result='BackgroundImageFix' />
+        <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape' />
+        <feGaussianBlur result='effect1_foregroundBlur_40004022_5050' stdDeviation='26.5' />
+      </filter>
+      <filter
+        colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
+        height='204'
+        id='h'
+        width='268'
+        x='-33'
+        y='-93'
+      >
+        <feFlood floodOpacity='0' result='BackgroundImageFix' />
+        <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape' />
+        <feGaussianBlur result='effect1_foregroundBlur_40004022_5050' stdDeviation='23' />
+      </filter>
+      <filter
+        colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
+        height='244'
+        id='i'
+        width='292'
+        x='88'
+        y='-100'
+      >
+        <feFlood floodOpacity='0' result='BackgroundImageFix' />
+        <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape' />
+        <feGaussianBlur result='effect1_foregroundBlur_40004022_5050' stdDeviation='26.5' />
+      </filter>
+      <filter
+        colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
+        height='208'
+        id='j'
+        width='292'
+        x='88'
+        y='-100'
+      >
+        <feFlood floodOpacity='0' result='BackgroundImageFix' />
+        <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape' />
+        <feGaussianBlur result='effect1_foregroundBlur_40004022_5050' stdDeviation='26.5' />
+      </filter>
+      <filter
+        colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
+        height='206'
+        id='k'
+        width='252'
+        x='-15'
+        y='-111'
+      >
+        <feFlood floodOpacity='0' result='BackgroundImageFix' />
+        <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape' />
+        <feGaussianBlur result='effect1_foregroundBlur_40004022_5050' stdDeviation='23' />
+      </filter>
+      <clipPath id='a'>
+        <path d='M0 0h328v220H0z' fill='#fff' />
+      </clipPath>
+    </defs>
+  </svg>
+);
---
+++ app/_components/Bento/OrbitingStack.tsx
@@ -0,0 +1,79 @@
+import React, { Fragment } from 'react';
+
+import {
+  AngularIcon,
+  LaravelIcon,
+  NextjsIcon,
+  NodejsIcon,
+  NuxtIcon,
+  ReactIcon,
+  SvelteIcon,
+  VuejsIcon
+} from '@/components/icons';
+import { cn } from '@/lib/utils';
+
+type OrbitingCirclesProps = Omit<React.ComponentProps<'div'>, 'children'>;
+
+const TECHNOLOGIES = [
+  [VuejsIcon, ReactIcon],
+  [AngularIcon, SvelteIcon],
+  [NuxtIcon, NextjsIcon],
+  [NodejsIcon, LaravelIcon]
+] as const;
+
+export const OrbitingStack = ({ className, ...props }: OrbitingCirclesProps) => (
+  <div
+    className={cn(
+      'relative flex aspect-square size-75 items-center justify-center select-none',
+      className
+    )}
+    {...props}
+  >
+    {TECHNOLOGIES.map((orbit, orbitIndex) => {
+      const radius = 44 + orbitIndex * 30;
+      return (
+        <Fragment key={radius}>
+          <svg
+            className='pointer-events-none absolute inset-0 size-full'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <circle
+              className='stroke-black/10 stroke-1 dark:stroke-white/10'
+              cx='50%'
+              cy='50%'
+              fill='none'
+              r={radius}
+            />
+          </svg>
+          {orbit.map((Icon, index) => {
+            const angle = 180 * index + orbitIndex * 15;
+            return (
+              <div
+                key={angle}
+                className={cn(
+                  'absolute flex transform-gpu animate-[orbit_linear_infinite] items-center justify-center',
+                  'size-6 rounded-full border border-black/10 bg-background dark:border-white/10 [&>svg]:size-4',
+                  'hover:paused',
+                  orbitIndex % 2 === 0 && 'direction-[reverse]'
+                )}
+                style={
+                  {
+                    animationDuration: `${20 + orbitIndex * 2}s`,
+                    '--radius': `${radius}px`,
+                    '--angle': `${angle}deg`
+                  } as React.CSSProperties
+                }
+              >
+                <Icon />
+              </div>
+            );
+          })}
+        </Fragment>
+      );
+    })}
+
+    <div className='flex size-10 items-center justify-center rounded-full bg-primary font-pixelify-sans text-[24px]/7 font-bold tracking-wide text-primary-fg'>
+      jb
+    </div>
+  </div>
+);
