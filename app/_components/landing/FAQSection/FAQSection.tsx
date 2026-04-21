f01f580f0b7e6bd4902c30aefdc7e6ab28167897
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2026-04-21 23:20:01 +0500

    landing 🌠 remove faq animations

--- app/_components/landing/FAQSection/FAQSection.tsx
+++ app/_components/landing/FAQSection/FAQSection.tsx
@@ -1,5 +1,4 @@
-'use client';
-import { motion } from 'motion/react';
+import * as motion from 'motion/react-client';
 
 import { MatrixGrid } from '@/components/common';
 import { AvoidCursor } from '@/components/common/motion';
@@ -15,48 +14,33 @@ import { IntlText } from '@/intl';
 import { FAQ_ITEMS } from './constants';
 
 export const FAQSection = () => (
-  <section className='relative flex flex-col gap-8 sm:gap-10'>
-    <Typography asChild pixelify variant='display'>
-      <motion.h2
-        transition={{
-          duration: 0.6
-        }}
-        initial={{ opacity: 0, y: '20%' }}
-        viewport={{ once: true }}
-        whileInView={{ opacity: 1, y: 0 }}
-      >
-        <IntlText path='page.home.section.faq.title' />
-      </motion.h2>
+  <motion.section
+    transition={{
+      duration: 0.6
+    }}
+    className='relative flex flex-col gap-8 sm:gap-10'
+    initial={{ opacity: 0, y: '20%' }}
+    viewport={{ once: true }}
+    whileInView={{ opacity: 1, y: 0 }}
+  >
+    <Typography pixelify as='h2' variant='display'>
+      <IntlText path='page.home.section.faq.title' />
     </Typography>
 
     <Accordion collapsible defaultValue={FAQ_ITEMS[0].question} type='single'>
-      {FAQ_ITEMS.map(({ answer, question }, index) => (
-        <AccordionItem asChild key={question} value={question}>
-          <motion.div
-            transition={{
-              delay: (index + 1) * 0.3,
-              duration: 0.6
-            }}
-            initial={{ opacity: 0, x: index % 2 === 0 ? '30%' : '-30%' }}
-            viewport={{ once: true }}
-            whileInView={{ opacity: 1, x: 0 }}
-          >
-            <AccordionTrigger>
-              <IntlText path={question} />
-            </AccordionTrigger>
-            <AccordionContent>
-              <IntlText path={answer} />
-            </AccordionContent>
-          </motion.div>
+      {FAQ_ITEMS.map(({ answer, question }) => (
+        <AccordionItem key={question} value={question}>
+          <AccordionTrigger>
+            <IntlText path={question} />
+          </AccordionTrigger>
+          <AccordionContent>
+            <IntlText path={answer} />
+          </AccordionContent>
         </AccordionItem>
       ))}
     </Accordion>
 
-    <div
-      aria-hidden
-      suppressHydrationWarning
-      className='pointer-events-none absolute inset-0 -z-1 select-none'
-    >
+    <div aria-hidden className='pointer-events-none absolute inset-0 -z-1 select-none'>
       <AvoidCursor
         whileInView={{
           scale: 1,
@@ -77,5 +61,5 @@ export const FAQSection = () => (
         />
       </AvoidCursor>
     </div>
-  </section>
+  </motion.section>
 );
