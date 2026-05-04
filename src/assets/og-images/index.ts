48538995c4593b1884d0f764c8d6612ced123c5f
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2026-05-04 15:47:32 +0500

    og 🌀 og templates

--- src/assets/og-images/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as guides } from './guides.png';
-export { default as landing } from './landing.png';
-export { default as tasks } from './tasks.png';
---
+++ src/assets/og-templates/index.ts
@@ -0,0 +1,2 @@
+export { default as guide } from './guide.png';
+export { default as task } from './task.png';
