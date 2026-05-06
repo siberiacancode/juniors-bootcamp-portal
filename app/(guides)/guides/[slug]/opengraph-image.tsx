fd3bd45d6e23ead805763e1696b84e2b054c2e94
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2026-05-06 21:57:18 +0500

    og 🌠 og improvements

--- app/(guides)/guides/[slug]/opengraph-image.tsx
+++ app/(guides)/guides/[slug]/opengraph-image.tsx
@@ -1,40 +1,38 @@
 import { ImageResponse } from 'next/og';
 
-import { getNunitoBold } from '@/og/fonts';
-import { getOGTemplateSrc } from '@/og/templates';
+import { getNunitoBold, getOGTemplateBase } from '@/og';
 
 import { getGuideModule } from '../../_helpers';
 
-const Image = async ({ params }: { params: Promise<{ slug: string }> }) => {
+interface GuideOpenGraphImageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+const Image = async ({ params }: GuideOpenGraphImageProps) => {
   const { slug } = await params;
 
   const title = (await getGuideModule(slug)).metadata.title;
 
-  const src = await getOGTemplateSrc('guide');
+  const src = await getOGTemplateBase('guide');
   const nunito = await getNunitoBold();
 
   return new ImageResponse(
-    <div
-      style={{
-        display: 'flex',
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        background: 'white',
-        padding: '100px 80px',
-        height: '100%',
-        width: '100%'
-      }}
-    >
-      <img
+    <>
+      <img src={src} />
+      <div
         style={{
-          width: '388px',
-          height: '153px'
+          fontSize: '60px',
+          lineHeight: '68px',
+          position: 'absolute',
+          bottom: '100px',
+          left: '80px'
         }}
-        src={src}
-      />
-      <div style={{ fontSize: '60px', lineHeight: '68px' }}>{title}</div>
-    </div>,
+      >
+        {title}
+      </div>
+    </>,
     {
       fonts: [nunito]
     }
--- src/og/templates/index.ts
+++ src/og/templates/index.ts
@@ -5,7 +5,7 @@ import { join } from 'node:path';
 
 export type OGTemplatePath = 'guide' | 'task';
 
-export const getOGTemplateSrc = async (path: OGTemplatePath) => {
+export const getOGTemplateBase = async (path: OGTemplatePath) => {
   const data = await readFile(join(process.cwd(), 'src/og/templates', `${path}.png`), 'base64');
   return `data:image/png;base64,${data}`;
 };
